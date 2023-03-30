20ec2ce29836e8691e1383d5b1e78f231b033f3d
kevinhng77 <62964063+kevinhng77@users.noreply.github.com>
2023-03-30 15:55:08 -0700

    got arrays to work

--- script.js
+++ script.js
@@ -13,8 +13,26 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-
+const Listofoptions = [];
 var Upper = Lower = Numbers = Special = "no";
+const Capital = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+const notCapital = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+const numeric = [0,1,2,3,4,5,6,7,8,9];
+const specialchar = [ "+", "-", "&&", "||", "!", "(", ")", "{", "}", "[", "]", "^", "~", "*", "?", ":","\"","\\"];
+
+function question4() {
+  Passlength = window.prompt("what is the length of the password you want? Please input number between 8 and 128");
+  Passlength= parseInt(Passlength);
+  if (Passlength >= 8 && Passlength <= 128) {
+    return;
+  }
+  else {
+    alert("Please input number between 8 and 128")
+    question4();
+  }
+}
+
+
 function generatePassword(){
   if (confirm('Do you want upper case letters?')) {
     Upper = "yes"
@@ -30,30 +48,21 @@ function generatePassword(){
     Special = "yes"
   }
   question4();
-}
 
-var emptyPass= '';
-var Listofoptions = [];
-const Capital = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
-const notCapital = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
-const numeric = [0,1,2,3,4,5,6,7,8,9];
-const specialchar = [ "+", "-", "&&", "||", "!", "(", ")", "{", "}", "[", "]", "^", "~", "*", "?", ":","\"","\\"];
-
-function genpass(){
   if (Upper = "yes"){
-    Listofoptions.concat(Capital);
+    Listofoptions.push(...Capital);
     console.log(Listofoptions);
   }
   if (Lower = "yes"){
-    Listofoptions.concat(notCapital);
+    Listofoptions.push(...notCapital);
     console.log(Listofoptions);
   }
   if (Numbers = "yes"){
-    Listofoptions.concat(numeric);
+    Listofoptions.push(...numeric);
     console.log(Listofoptions);
   }
   if (Special = "yes"){
-    Listofoptions.concat(specialchar);
+    Listofoptions.push(...specialchar);
     console.log(Listofoptions);
   }
   for (var i =0 ; i < Passlength; i++){
@@ -61,19 +70,32 @@ function genpass(){
     console.log(random, Listofoptions[random]);
   }
     
-
 }
+// function genpass(){
+//   if (Upper = "yes"){
+//     Listofoptions.concat(Capital);
+//     console.log(Listofoptions);
+//   }
+//   if (Lower = "yes"){
+//     Listofoptions.concat(notCapital);
+//     console.log(Listofoptions);
+//   }
+//   if (Numbers = "yes"){
+//     Listofoptions.concat(numeric);
+//     console.log(Listofoptions);
+//   }
+//   if (Special = "yes"){
+//     Listofoptions.concat(specialchar);
+//     console.log(Listofoptions);
+//   }
+//   for (var i =0 ; i < Passlength; i++){
+//     const random = Math.floor(Math.random() * Listofoptions.length);
+//     console.log(random, Listofoptions[random]);
+//   }
+    
+
+// }
 
 
-function question4() {
-  Passlength = window.prompt("what is the length of the password you want? Please input number between 8 and 128");
-  Passlength= parseInt(Passlength);
-  if (Passlength >= 8 && Passlength <= 128) {
-    return;
-  }
-  else {
-    alert("Please input number between 8 and 128")
-    question4();
-  }
-}
+
 
